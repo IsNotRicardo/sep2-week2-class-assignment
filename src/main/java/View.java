89f0fab00522d021f1b0ea86497e16051ae71955
ItsNotRicardo <142903523+IsNotRicardo@users.noreply.github.com>
2025-03-27 14:50:50 +0200

    Increase window width

--- src/main/java/View.java
+++ src/main/java/View.java
@@ -8,7 +8,7 @@ public class View extends Application {
     public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/hello-view.fxml"));
         stage.setScene(new Scene(loader.load()));
-        stage.setWidth(300);
+        stage.setWidth(350);
         stage.show();
     }
 }
